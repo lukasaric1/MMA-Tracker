105ec7ad3cd3fe5fe220129ec3f5a466da2ae955
Luka Saric <luka.saric1999@gmail.com>
2024-12-05 23:30:03 +0100

    Activating Retrofit

--- app/src/main/java/com/example/mma_application/EventsPage.kt
+++ app/src/main/java/com/example/mma_application/EventsPage.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
 import androidx.compose.runtime.*
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
